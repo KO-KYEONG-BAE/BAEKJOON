bf6b205bbcf187c963c46b1c822c213ef5ae0af9
KO-KYEONG-BAE <kkb2782@naver.com>
2025-02-24 15:56:21 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace baekjoon
+{
+    
+
+    class System
+    {
+        static void Main(string [] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+
+            int result1 = int.Parse(a) + int.Parse(b) - int.Parse(c);
+            int result2 = int.Parse(a + b) - int.Parse(c);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+}
