00e6f8b13670770808cdc06168526e2db15b53c0
KO-KYEONG-BAE <kkb2782@naver.com>
2025-03-15 10:01:15 +0900

    baekjoon 2720

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
 ﻿using System;
 
-class Baekjoon2745
+namespace baekjoon
 {
-    static void Main(string[] args)
+    class Program
     {
-        string[] input = Console.ReadLine().Split();
-        string number = input[0].ToUpper();  // 대문자로 변환 (A~Z 처리)
-        int baseNum = int.Parse(input[1]);  // 진법 변환할 숫자
-        int sum = 0;
-
-        for (int i = 0; i < number.Length; i++)
+        static void Main(string[] args)
         {
-            int value;
-
-            if (number[i] >= '0' && number[i] <= '9') 
-            {
-                value = number[i] - '0';  // 숫자 (0~9)
-            }
-            else 
-            {
-                value = number[i] - 'A' + 10;  // 알파벳 (A~Z → 10~35)
+            int tCase = int.Parse(Console.ReadLine());
+            int[] CaseInput = new int[tCase];
+            for (int i = 0; i < tCase; i++){
+                CaseInput[i] = int.Parse(Console.ReadLine());
             }
 
-            sum += value * (int)Math.Pow(baseNum, number.Length - 1 - i);
+            for (int i = 0; i < tCase; i++){
+                Console.WriteLine("{0} {1} {2} {3}", CaseInput[i] / 25, CaseInput[i] % 25 / 10, CaseInput[i] % 25 % 10 / 5, CaseInput[i] % 25 % 10 % 5 / 1);
+            }
         }
-
-        Console.WriteLine(sum);
     }
 }
